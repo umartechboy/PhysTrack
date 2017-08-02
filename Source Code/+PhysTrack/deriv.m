d122a9374afcbbb2a449e34c8d9ec141f6afc13a
Muhammad Umar Hassan <umar.hassn@lums.edu.pk>
2017-08-02 10:41:08 +0500

    minor changes

--- Source Code/+PhysTrack/deriv.m
+++ Source Code/+PhysTrack/deriv.m
@@ -1,13 +1,7 @@
 function [xd,yd] = deriv(xdata, ydata, order)
     h = xdata(2) - xdata(1);
     if order == 1
-        %yd = double(-ydata(4:end)  + 8 * ydata(3:end - 1) - 8 * ydata(2:end - 2) + ydata(1:end - 3))  / 12.0 / double(xdata(2) - xdata(1));                        
-        %yd = double(-ydata(1:end-5) + 9 * ydata(2:end-4) - 45 * ydata(3:end-3) + 45 * ydata(4:end-2) - 9 * ydata(5:end-1) + ydata(6:end))  / 60.0 / double(xdata(2) - xdata(1));                        
-        %yd = [];
-        %for i = 1:(length(ydata)-3)
         yd = (2*ydata(4:end)-9*ydata(3:end - 1)+18*ydata(2:end - 2)- 11*ydata(1:end - 3))/(6*h);
-        %xd(i) = xdata(i);
-        %end
     end
     if order == 2
          yd = (-1*ydata(4:end)+4*ydata(3:end-1)-5*ydata(2:end-2)+2*ydata(1:end-3))/(h^2);
@@ -18,10 +12,9 @@ function [xd,yd] = deriv(xdata, ydata, order)
     if order == 4
         yd = (ydata(5:end)      - 4 * ydata(4:end - 1) + 6 * ydata(3:end - 2) - 4 * ydata(2:end - 3) + ydata(1:end - 4))  / (xdata(2) - xdata(1))^4;        
     end
-    %if order == 1 || order == 3
-    if order == 1 || order == 2
+    
+    if order ~= 4
         xd = (xdata(2) - xdata(1)) * 1.5 + xdata(1:end - 3);
-        %xd = xdata(3:end - 2);
     else
         xd = xdata(3:end - 2);  
     end
